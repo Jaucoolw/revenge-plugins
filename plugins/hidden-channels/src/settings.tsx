8eac505c0e47deabb9352b7f2adf9817847962e2
Jaucoolw <jaycoolw011@gmail.com>
2026-09-05 04:34:37 -0400

    Refactor imports and simplify onValueChange handlers

--- plugins/hidden-channels/src/settings.tsx
+++ plugins/hidden-channels/src/settings.tsx
@@ -1,60 +1,37 @@
-import {
-    React,
-    ReactNative as RN,
-} from "@vendetta/metro/common";
-
+import { ReactNative as RN } from "@vendetta/metro/common";
 import { useProxy } from "@vendetta/storage";
 import { storage } from "@vendetta/plugin";
 import { Forms } from "@vendetta/ui/components";
-import {
-    getAssetIDByName,
-} from "@vendetta/ui/assets";
+import { getAssetIDByName } from "@vendetta/ui/assets";
 
-const {
-    FormRow,
-    FormSection,
-    FormSwitchRow,
-} = Forms;
+const { FormRow, FormSection, FormSwitchRow } = Forms;
 
 export function Settings() {
     useProxy(storage);
 
     return (
-        <RN.ScrollView
-            style={{ flex: 1 }}
-        >
+        <RN.ScrollView style={{ flex: 1 }}>
             <FormSection
                 title="Options"
                 titleStyleType="no_border"
             >
                 <FormSwitchRow
                     label="Show Lock Icon"
-
                     subLabel="Show a lock icon to the right of hidden channel names."
-
                     leading={
                         <FormRow.Icon
-                            source={getAssetIDByName(
-                                "ic_lock"
-                            )}
+                            source={getAssetIDByName("ic_lock")}
                         />
                     }
-
-                    onValueChange={(value) => {
-                        storage.showIcon =
-                            value;
-                    }}
-
-                    value={
-                        storage.showIcon
+                    onValueChange={(value) =>
+                        (storage.showIcon = value)
                     }
+                    value={storage.showIcon}
                 />
 
                 <FormSwitchRow
                     label="Show Popup on Hidden Channels"
-
                     subLabel="Toggle the information popup that appears when selecting hidden channels."
-
                     leading={
                         <FormRow.Icon
                             source={getAssetIDByName(
@@ -62,15 +39,10 @@ export function Settings() {
                             )}
                         />
                     }
-
-                    onValueChange={(value) => {
-                        storage.showPopup =
-                            value;
-                    }}
-
-                    value={
-                        storage.showPopup
+                    onValueChange={(value) =>
+                        (storage.showPopup = value)
                     }
+                    value={storage.showPopup}
                 />
             </FormSection>
         </RN.ScrollView>
